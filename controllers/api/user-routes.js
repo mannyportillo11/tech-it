d9005d85bf562a6afe75988e2351745548f9f008
Man E <mannyportillo77@gmail.com>
2021-10-19 16:46:14 -0600

    inputed user-routes, might need to go back and add some more code later

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,114 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+      attributes: { exclude: ['password'] }
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+        //no need for votes, borrowed code from modules
+        // {
+        //   model: Post,
+        //   attributes: ['title'],
+        //   through: Vote,
+        //   as: 'voted_posts'
+        // }
+      ]
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+//update
+router.put("/:id", (req, res) => {
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbUserData) => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// delete 
+router.delete("/:id", (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
