58e79f431eb275afea5c3b7975fd30b93484d205
Man E <mannyportillo77@gmail.com>
2021-10-19 16:17:56 -0600

    initialized express session in server file and created schema file

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const hbs = exphbs.create({});
 
